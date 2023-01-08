28ec3dffc11cd8f0c8937de6eb3f1d961c78b264
Rikin <mogofa8686@galotv.com>
2023-01-08 16:56:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const DUMMY_EXPENSES = [{
+  id: "e1",
+  title: "Click 'Add New Expense to add new expense'",
+  amount: 0,
+  date: new Date(2022, 7, 14),
+}];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
